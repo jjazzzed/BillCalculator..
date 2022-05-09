84ec2df463071267ed50c37763824286fbc7752f
JAZLYNN NG HUI YING <21013059@myrp.edu.sg>
2022-05-09 16:09:33 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id21013059/billcalculator/MainActivity.java
@@ -0,0 +1,26 @@
+package sg.edu.rp.c346.id21013059.billcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView totalBill;
+
+
+
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+
